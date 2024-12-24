f2d473d9db9b9e791323097997c034dfb6a7aa6a
Sreejithks@2000 <sreejithksgupta2255@gmial.com>
2024-12-24 15:22:28 +0530

    added basic validation

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,62 @@
-function hellofn(){
-    alert("hello world");
+function validateregform() {
+    let formdict = {};
+    formdict["name"] = regform.name.value;
+    formdict["email"] = regform.email.value;
+    formdict["age"] = regform.age.value;
+    formdict["place"] = regform.place.value;
+    formdict["mob"] = regform.mob.value;
+    formdict["gender"] = regform.gender.value;
+    formdict["interest"] = getchecked("interest");
+    formdict["mode"] = regform.mode.value;
+    
+    let errorlist =[]
+
+    Object.entries(formdict).forEach(([key, value]) => {
+        if (value == "") {
+            errorlist.push(key)
+        }
+    });
+    console.log(errorlist.length);
+
+   if(errorlist.length == 0) { 
+    return true ;
+   }
+   else {
+    alert ("please enter your "+errorlist[0]);
+    return false
+   }
+
+}
+
+function getchecked(field) {
+    let checkboxes =
+        document.getElementsByName(field);
+    let result = [];
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            result.push(checkboxes[i].value)
+        }
+    }
+    return result;
+}
+
+function validatecontact(){
+    let name = contactform.name.value;
+    let email = contactform.email.value;
+    let address = contactform.address.value;
+    
+    if (name==''){
+        alert("enter name");
+         return false;
+    }
+    else if (email==''){
+        alert("enter name");
+         return false;
+    }
+    else if (address==''){
+        alert("enter name");
+         return false;
+    }
+    return true;
+
 }
